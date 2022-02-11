5d2321fd96bf2601806d09094a74c9a40f3da30a
GeniusDP <zaranikbz@gmail.com>
2022-02-11 21:21:38 +0200

    Now all words are parsed by pages

--- Task2/main.cpp
+++ Task2/main.cpp
@@ -42,6 +42,12 @@ using namespace std;
     (is hardcoded)
 */
 
+struct WordProcessor{
+    string word;
+    int pages[101];
+    int currentNumberOfPages = 0;
+};
+
 int main()
 {
     string stopWords[] =
@@ -50,6 +56,66 @@ int main()
          "onto", "into", "are", "but", "is",
          "and", "not", "or"
         };
+    ifstream in("input.txt");
+    const int PAGE_SIZE_IN_ROWS = 5;
 
+
+    int allWordsCapacity = 100;
+    int allWordsLength = 0;
+    WordProcessor* allWords = new WordProcessor[allWordsCapacity];
+
+    string currentLine;
+    while( in.peek() != EOF ){
+        int cnt = 0;
+        while( ++cnt <= PAGE_SIZE_IN_ROWS && in.peek() != EOF ){
+            getline(in, currentLine);
+            //cout << currentLine << endl;
+
+            /*
+                split
+            */
+
+            int left, right;
+            left = right = 0;
+            while( currentLine[left] != '\0' ){
+                if( currentLine[left] != ' ' ){
+                    right = left;
+                    while( currentLine[right] != ' ' && currentLine[right] != '\0' ){
+                        right++;
+                    }
+                    string currentWord = "";
+                    int currentWordSize = right-left;
+                    while( left < right ){
+                        currentWord += currentLine[left];
+                        left++;
+                    }
+                    if( currentWord[currentWordSize-1] == '.' || currentWord[currentWordSize-1] == ','
+                       || currentWord[currentWordSize-1] == '!' || currentWord[currentWordSize-1] == '?'
+                       || currentWord[currentWordSize-1] == ':' || currentWord[currentWordSize-1] == ';'){
+                        //so that is why we`re deleting last character
+                        string tmp = "";
+                        for(int i = 0; i < currentWordSize-1; i++){
+                            tmp += currentWord[i];
+                        }
+                        currentWord = tmp;
+                    }
+                    cout << currentWord << "|";
+                }else{
+                    left++;
+                }
+            }
+
+
+
+
+        }
+    }
+
+    /*
+        sorting
+    */
+
+
+    in.close();
     return 0;
 }
